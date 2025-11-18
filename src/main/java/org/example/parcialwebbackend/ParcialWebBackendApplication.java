4cf3b698320b468c0094cb68da85e4cd0a0eb89a
Julian Diaz <alexdior59@gmail.com>
2025-11-18 18:19:51 -0500

    Initialize Spring Boot project with Maven configuration.

---
+++ src/main/java/org/example/parcialwebbackend/ParcialWebBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.parcialwebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcialWebBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parcialwebbackend/ParcialWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parcialwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
